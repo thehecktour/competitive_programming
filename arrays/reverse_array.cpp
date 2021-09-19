5bf36e05a73efa5435503729fbb93ef5ba1ac920
Fedorov <atiliogoulart@gmail.com>
2021-09-19 11:12:53 -0300

    Updates

---
+++ arrays/reverse_array.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+    int arr[] = {1,2,3,4,5,6,7,8,9};
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Array: " << endl;
+
+    for (int i=0; i<n; i++){
+        cout << arr[i] << endl;
+    }
+
+    reverse(arr,arr+n);
+
+    cout << "Reverse array: " << endl;
+
+    for (int i=0; i<n; i++){
+
+        cout << arr[i] << endl;
+
+    }
+
+}
