a1b79975cdb3bbe59e58f77e490f778a56d75453
Fedorov <atiliogoulart@gmail.com>
2021-09-20 12:35:52 -0300

    Update

--- leetcode/review/TwoSum.py
+++
@@ -1,9 +0,0 @@
-arr = [int(i) for i in input().split()]
-target = int(input())
-
-tamanho = len(arr)
-
-for i in range(tamanho -1):
-    for j in range(i+1, tamanho):
-        if arr[i] + arr[j] == target:
-            print("[{},{}]".format(arr[i],arr[j]))
