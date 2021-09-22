6aee9fae2ce7420b90d12073d55e929e2764c90b
Fedorov <atiliogoulart@gmail.com>
2021-09-22 16:53:59 -0300

    Review

---
+++ uri/review/1059.cpp
@@ -0,0 +1,16 @@
+#include <iomanip>
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+int main(){
+
+    for(int i=0; i<100; i++){
+        cout << i << endl;
+        if (i/2==0){
+            cout << i << endl;
+        }
+    }
+
+}
