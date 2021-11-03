153a5b7a96104a1e6d968c7fc90ff92f957e0fbe
Fedorov <atiliogoulart@gmail.com>
2021-11-02 23:28:21 -0300

    Update

--- data/ponteiros.cpp
+++ data/ponteiros.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
 
-       
+       int* ponteirinho = new int;
 
+       cin >> *ponteirinho;
+
+       cout << *ponteirinho << endl;
 }
