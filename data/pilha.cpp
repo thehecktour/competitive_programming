6ae9f0098a5f49cb35b49b4daae2d7f90dc2755a
Fedorov <atiliogoulart@gmail.com>
2021-11-06 22:42:41 -0300

    Done

--- data/pilha.cpp
+++ data/pilha.cpp
@@ -1,14 +1,43 @@
 #include <iostream>
-#include <stack>
-
+#include <cstdlib>
 using namespace std;
 
-unsigned long hash_function(char* str){
-                     
+
+#define SIZE 10
+
+class stack{
+       int *arr;
+       int top;
+       int capacity;
+
+public:
+
+       stack(int size=SIZE);
+       ~stack();
+
+       void push(int);
+       int pop();
+       int peek();
+       int size();  
+       bool isEmpty();
+       bool isFull();
+
+};
+
+stack::stack(int size){
+       arr = new int[size];
+       capacity = size;
+       top = -1;
 }
+
+stack::~stack(){
+       delete[] arr;
+}
+
+
 
 int main(){
 
-       
+              
 
 }
---
+++ uri/1070.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+       int a;
+
+       cin >> a;
+
+       for(int i=a; i<a+12 ;i++){
+              if(i%2!=0){
+                     cout << i << endl;
+              }   
+       }
+
+}
