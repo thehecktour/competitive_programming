6c49a1b073a4e195328e5468119986741d475e0f
Fedorov <atiliogoulart@gmail.com>
2021-11-07 10:19:35 -0300

    Update

--- uri/dont/3299.cpp
+++ uri/dont/3299.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
 
-       int a;
+       string a;
 
-       
+       cin >> a;
+
+       for(int i=0; i<a.length(); i++){
+              if(a[i] + a[i+1] == 13){
+                     cout << "sim" << endl;
+              }
+       }
 
 }
