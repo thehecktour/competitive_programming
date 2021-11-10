cafe660825a8e47fa7f338f6cdaaaa9b608d9b6b
Fedorov <atiliogoulart@gmail.com>
2021-11-10 19:37:16 -0300

    Done

--- rhama/atividade3.cpp
+++ rhama/atividade3.cpp
@@ -4,16 +4,58 @@ using namespace std;
 
 int main(){
 
-       int questao, alunos, respostasQuestoes, numeroQuestoes;
-       vector<int> listinhaRespostas;
+       int gabarito;
+       vector<int> ArrayGabarito, listaNotas;
+       int contador, valorNota, resultado;
+       int alunos, media, quantidadeAlunos, medida;
+       int nota1;
 
-       cout << "Quantas questões tem sua prova? " << endl;
-       cin>> numeroQuestoes;
+       contador = 0;
+       valorNota = 0;
+       quantidadeAlunos = 0;
 
-       for(int i=0; i<=numeroQuestoes; i++){
-              cout << "Digite a resposta correta da " << i+1 << "º questão: " << endl;
-              cin >> questao;
-              listinhaRespostas.push_back(questao);
+       while (true){
+              cout << "Digite o gabarito das 3 questões: " << endl;
+              cout << "Opções de cada questão: 1-A 2-B 3-C " << endl;
+              cin >> gabarito;
+              ArrayGabarito.push_back(gabarito);
+              contador+=1;  
+              if(contador == 3){
+                     break;
+              }
        }
+
+
+       cout << "Quantos alunos tem na classe? " << endl;
+       cin >> alunos;
+
+       while(true){
+              for(int i=0; i<3; i++){
+                     cout << "Digite a " << i+1 <<"º nota desse aluno: " << endl;
+                     cin >> nota1;
+                     if(nota1 == ArrayGabarito[i]){
+                            listaNotas.push_back(nota1);
+                            resultado +=1;
+                     }
+              }
+
+              for(int i=0; i<3; i++){
+                     media += listaNotas[i];
+              }
+
+              if(resultado>3){
+                     quantidadeAlunos +=1;
+              }
+
+              valorNota+=1;
+              if(valorNota == alunos){
+                     break;
+              }
+       }
+       
+       medida = (quantidadeAlunos/alunos);
+
+       cout << "Quantidade de alunos aprovados é : " << quantidadeAlunos << endl;
+       cout << "Porcentagem de alunos aprovados é de :" << medida << endl;
 
 }
