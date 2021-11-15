3e9840e32c4114175c52b9a33d88fe2c7a6dcb2a
Colaborador <colaborador@N093486.local>
2021-11-15 20:49:33 -0300

    Done

---
+++ uri/1001.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a,b, result;
+
+    cin >> a >> b;
+
+    result = (a+b);
+
+    cout << "X = " << result << endl;
+
+    return 0;
+
+}
---
+++ uri/1003.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a,b, result;
+
+    cin >> a >> b;
+
+    result = (a+b);
+
+    cout << "SOMA = " << result << endl;
+
+    return 0;
+
+}
