d4b2725e2a866fd3f1a66bf04c49e62fa843095d
Colaborador <colaborador@N093486.local>
2021-11-15 20:54:58 -0300

    Done

---
+++ uri/1004.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a,b, result;
+
+    cin >> a >> b;
+
+    result = (a*b);
+
+    cout << "PROD = " << result << endl;
+    
+    return 0;
+
+}
