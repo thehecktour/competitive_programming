6f32c3daac703d540f74f1cc8696083438a81f46
Hector <atiliogoulart@gmail.com>
2021-12-01 18:33:31 -0300

    Done 1589

---
+++ uri/1589.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int x;
+
+    cin >> x;
+
+    for(int i=0; i<x; i++){
+        int a,b;
+        cin >> a >> b ;
+        int resultado;
+        resultado = (a+b);
+        cout << resultado << endl;
+    }
+
+}
