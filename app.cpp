f3236e2c259a3cb712ac1b92f5042b9be5ec1580
Hector <atiliogoulart@gmail.com>
2021-12-08 06:47:34 -0300

    Done

--- app.cpp
+++ app.cpp
@@ -2,20 +2,6 @@
 #include <vector>
 using namespace std;
 
-struct Node{
-       public:
-              int valor;
-       Node * proximo;
-};
-
-void mostrarValor(Node * n){
-       while(n!=NULL){
-              cout << n->valor << " " << endl;
-              n = n->proximo;
-       }
-       
-}
-
 int main(){
        vector<int> listinha;
        cout  << "Digite o tamanho da sua lista encadeada: " << endl;
@@ -27,18 +13,9 @@ int main(){
               int item;
               cin >> item;
 
-              Node * valorPonteiro = NULL;
-
-              valorPonteiro = new Node();
-
-              valorPonteiro->valor = item;
-              valorPonteiro->proximo = NULL;
-
-              listinha.push_back(valorPonteiro->valor);
-
-              mostrarValor(valorPonteiro);
+              listinha.push_back(item);
        }
-       
+
 
        for(int i=0; i<size; i++){
               cout << listinha[i] << endl;
