7568a6bc05b5531d0ed59e30d14043fe33e25ed7
Hector <atiliogoulart@gmail.com>
2021-12-09 15:22:52 -0300

    Done 1115

---
+++ uri/1115.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    double x,y;
+
+    while(true){
+        cin >> x >> y;
+
+        if(x==0||y==0){
+            break;
+        }
+
+        if(x>0 && y>0){
+            cout << "primeiro" << endl;
+        }else if(x<0 && y>0){
+            cout << "segundo" << endl;
+        }else if(x<0 && y<0){
+            cout << "terceiro" << endl;
+        }else if(x>0 && y<0){
+            cout << "quarto" << endl;
+        }
+    }
+    
+
+}
