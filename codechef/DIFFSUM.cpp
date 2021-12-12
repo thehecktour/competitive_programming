0a9f956018aa3d1a0fc5a7c092e2a276d30f88ed
Hector <atiliogoulart@gmail.com>
2021-12-12 09:58:30 -0300

    Done

---
+++ codechef/DIFFSUM.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a,b;
+
+    cin >> a >> b;
+
+    if(a>b){
+        int r;
+        r = (a-b);
+        cout << r << endl;
+    }else{
+        int k;
+        k = (a+b);
+        cout << k << endl;
+    }
+
+}
