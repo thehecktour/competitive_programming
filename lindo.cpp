ecab4853eba108832fad666c80a50c9e166b97e3
Hector <atiliogoulart@gmail.com>
2022-01-30 13:30:38 -0300

    Updates questions

---
+++ lindo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+#include <map>
+#include <string.h>
+#include <string>
+using namespace std;
+
+
+int main(){
+
+    
+
+    return 0;
+}   
