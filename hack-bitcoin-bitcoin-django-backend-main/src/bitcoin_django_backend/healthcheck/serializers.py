8bd48024b70a7bf4096e947aa63490a91d8fd4e2
Atilio <atilio.jesus@grupoboticario.com.br>
2022-07-08 20:15:04 -0300

    Remove trash

--- hack-bitcoin-bitcoin-django-backend-main/src/bitcoin_django_backend/healthcheck/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-
-from bitcoin_django_backend.healthcheck.enums import Status
-
-
-class HealthcheckSerializer(serializers.Serializer):
-    status = serializers.ChoiceField(choices=Status.choices())
--- hack-bitcoin-bitcoin-django-backend-main/src/bitcoin_django_backend/healthcheck/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from bitcoin_django_backend.healthcheck.views import HealthCheckView
-
-urlpatterns = [
-    path("", HealthCheckView.as_view(), name="healthcheck"),
-]
