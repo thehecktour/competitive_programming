00e0bce6630899d7fc0d5bafdb386eb498817695
Atilio <atilio.jesus@grupoboticario.com.br>
2022-08-06 20:59:17 -0300

    Update folder structure

--- lindo3.cpp
+++ lindo3.cpp
@@ -3,27 +3,21 @@
 #include <algorithm>
 #define ll long long int
 #define pb push_back
- 
+
 using namespace std;
 
 int main() {    
-    
-    string f;
-    int resposta, contador = 0;
 
-    cin >> f;
-    
-    resposta = 0;
-    contador = f.length()-1;
+    int k;
+    cin >> k;
 
-    for(int i=0; i<f.length(); i++){
-        if(f[i]=='1'){
-            resposta+=pow(2,contador);
-        }
-        contador--;
+    while(k--){
+        int a,b;
+
+        cin >> a >> b;
+
+        
     }
 
-    cout << resposta << endl;
-
-    return 0;
+    return 0;   
 }
