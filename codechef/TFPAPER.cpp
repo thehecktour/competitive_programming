89d81ca054251891d5674f52661605088b6f5463
Atilio <atilio.jesus@grupoboticario.com.br>
2022-08-08 18:53:54 -0300

    Add one more question codechef

---
+++ codechef/TFPAPER.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#define ll long long int
+#define pb push_back
+
+using namespace std;
+
+int main() {    
+
+    int k;
+
+    cin >> k;
+    
+    while(k--){
+        int a,b;
+
+        cin >> a >> b;
+
+        cout << a-b << endl;
+    }
+
+    return 0;   
+}
