9b521eb34cd4aad67f5de3f0efe85fe3ac61757a
Atilio <atilio.jesus@grupoboticario.com.br>
2022-08-09 20:06:51 -0300

    Update folder structure

--- lindo3.cpp
+++ lindo3.cpp
@@ -7,28 +7,7 @@
 using namespace std;
 
 int main() {    
-
-    int k;
-
-    cin >> k;
-
-    while(k--){
-        string f;
-        int contadorA = 0, contadorB = 0;
-        cin >> f;
-        for(int i=0; i<f.length(); i++){
-            if((f[i]=='A'&&f[i+1]=='B')||f[i]=='B'&&f[i+1]=='A'){
-                contadorA++;
-                i++;
-            }
-        }
-
-        if(contadorA!=(f.length()/2)){
-            cout << "NO" << endl;
-        }else{
-            cout << "YES" << endl;
-        }
-    }
+    
 
     return 0;   
 }
