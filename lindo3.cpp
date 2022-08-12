d16e05faa748a62b60bb1744bf6f1bf84f2dde58
Atilio <atilio.jesus@grupoboticario.com.br>
2022-08-12 20:44:21 -0300

    6° codechef's question

--- lindo3.cpp
+++ lindo3.cpp
@@ -13,16 +13,9 @@ int main() {
     cin >> k;
 
     while(k--){
-        float a;
-        cin >> a;
-
-        if(a<1500){
-            cout << a*2 << endl;
-        }else{
-            cout << fixed << setprecision(2);
-            cout << 500+((a*98)/100)+a << endl;
-        }
+        int a; 
     }
+
 
     return 0;   
 }
