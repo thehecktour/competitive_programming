6a43cecb11d98d4282408fbb1952dd3e6c8c1ec0
Atilio <atilio.jesus@grupoboticario.com.br>
2022-08-22 19:20:11 -0300

    4° of day

---
+++ codechef/HEIGHTRATION.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#define ll long long int
+#define pb push_back
+
+using namespace std;
+
+int main() {  
+    
+    int a,b;
+
+    cin >> a >> b;
+
+    cout << max(a,b) << endl;
+    return 0;   
+}
