0b62692b17a2ba13028109c811164721f9e23f11
Atilio <atilio.jesus@grupoboticario.com.br>
2022-08-27 15:07:22 -0300

    First codeforces question of today

---
+++ codeforces/630A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#define ll long long int
+#define pb push_back
+
+using namespace std;
+
+int main() {    
+
+    int k;
+    cin >> k;
+
+    cout << 25 << endl;
+    
+    return 0;   
+}
