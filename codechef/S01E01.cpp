3b7a691545bf4ae76a970fe4a50cda9caf09d523
Atilio <atilio.jesus@grupoboticario.com.br>
2022-09-07 20:16:35 -0300

    3° of day

---
+++ codechef/S01E01.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#define ll long long int
+#define pb push_back
+
+using namespace std;
+
+int main() {    
+    
+    int l;
+
+    cin >> l;
+
+    while(l--){
+        int f;
+        cin >> f;
+        if(f<21){
+            cout << "NO" << endl;
+        }else{
+            cout << "YES" << endl;
+        }
+    }
+    return 0;   
+}
