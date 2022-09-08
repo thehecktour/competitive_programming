ee2643ae8c6e0c8a9bbe58eb3b93dca49d1bc306
Atilio <atilio.jesus@grupoboticario.com.br>
2022-09-08 09:30:54 -0300

    1° of day

---
+++ codechef/FEVER.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#define ll long long int
+#define pb push_back
+
+using namespace std;
+
+int main() {    
+    
+    int k;
+
+    cin >> k;
+
+    while(k--){
+        int f;
+        cin >> f;
+        if(f>98){
+            cout << "YES" << endl;
+        }else{
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;   
+}
