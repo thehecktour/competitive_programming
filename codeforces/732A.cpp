060301ea8d6b22fb36d37da030dd4210dfa522e1
Atilio <atilio.jesus@grupoboticario.com.br>
2022-09-08 19:33:09 -0300

    2° of day from CodeForces

--- codeforces/732A.cpp
+++ codeforces/732A.cpp
@@ -3,27 +3,27 @@
 #include <algorithm>
 #define ll long long int
 #define pb push_back
-
+ 
 using namespace std;
-
+ 
 int main() {    
-
+ 
     int a,b;
-
+ 
     cin >> a >> b;
-
+ 
     int cont = 1;
-
+ 
     while(true){
         if((((a*cont)-b)%10==0)||((a*cont)%10==0)){
             cout << cont << endl;
             break;
         }else{
             cont++;
-
+ 
         }
-
+ 
     }
-
+ 
     return 0;   
 }
