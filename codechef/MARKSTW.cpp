1ed293ac0f2e40787aa645a38703ffaf7114b8c9
Atilio <atilio.jesus@grupoboticario.com.br>
2022-09-22 19:17:40 -0300

    2° of day Codechef

---
+++ codechef/MARKSTW.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#define ll long long int
+#define pb push_back
+
+using namespace std;
+
+int main() {    
+
+    int a,b;
+
+    cin >> a >> b;
+
+    if((a)>=(b*2)){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+    return 0;   
+}
