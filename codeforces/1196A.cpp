a263f6aec3edf1091d00ce14d608e33c06903f4f
Atilio <atilio.jesus@grupoboticario.com.br>
2022-10-03 10:25:30 -0300

    2° of day in Codeforces

---
+++ codeforces/1196A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#define ll long long int
+#define pb push_back
+
+using namespace std;
+
+int main() {    
+
+    int k;
+    cin >> k;
+
+    while(k--){
+        ll a,b,c;
+
+        cin >> a >> b >> c;
+
+        cout << (a+b+c)/2 << endl;
+    }  
+    return 0;   
+}
